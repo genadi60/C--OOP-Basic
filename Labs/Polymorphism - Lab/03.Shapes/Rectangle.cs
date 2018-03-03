9b7cef2363cfb5f60c24b2dcfe2c91c3581fd522
genadi60 <genadimihaylov@gmail.com>
2018-03-03 10:56:37 +0200

    Continue modifing files

---
+++ Labs/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿public class Rectangle : Shape
+{
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public double Height
+    {
+        get => height;
+        private set => height = value;
+    }
+
+    public double Width
+    {
+        get => width;
+        private set => width = value;
+    }
+
+
+    public override double CalculatePerimeter()
+    {
+        var result = (this.Height + this.Width) * 2;
+        return result;
+    }
+
+    public override double CalculateArea()
+    {
+        var result = this.Height * this.Width;
+        return result;
+    }
+
+    public sealed override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
